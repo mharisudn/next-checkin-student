02c61c0bb100cbb3a2f13a70d32889f2274fe77b
Muhammad Harisuddin <abukaukab.assalam@gmail.com>
2025-07-10 08:06:59 +0700

    fix: auth type again

---
+++ src/types/next-auth.d.ts
@@ -0,0 +1,25 @@
+// types/next-auth.d.ts
+import NextAuth from 'next-auth';
+
+declare module 'next-auth' {
+  interface User {
+    id: string;
+    role: string;
+  }
+
+  interface Session {
+    user: {
+      id: string;
+      name?: string | null;
+      email?: string | null;
+      role: string;
+    };
+  }
+}
+
+declare module 'next-auth/jwt' {
+  interface JWT {
+    id: string;
+    role: string;
+  }
+}
